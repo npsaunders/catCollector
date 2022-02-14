1b000af9e3be12a30c722700dd835e8ffd9591ee
Gr8ness <pmleechapman@gmail.com>
2022-02-14 14:31:51 -0500

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4),
+    Cat('Rocket', 'Siamese', 'looks like rufus from kim posibble', 70),
+]
+
+def cats_index(request):
+    return render(request,'cats/index.html', { 'cats': cats})
